0ef321b1f6e7032dfec36b00590434055ccb2fe5
jsotoca <jsotoca@unprg.edu.pe>
2021-05-01 22:09:46 -0500

    Se creo modelo Libro

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    
+    nombre = models.CharField(max_length= 55)
+    editorial = models.CharField(max_length= 65)
+    nro_paginas = models.IntegerField()
+    anio_publicacion = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre} - {self.editorial}'
+    
